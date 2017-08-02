778b1a089ffd48b5fa5ea8f29dd496be5a81f142
Nate Villegas <natemv@gmail.com>
2017-08-02 13:48:59 -0700

    initial code

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendsArray = [
+ {
+    "name":"Nate",
+    "photo":"https://pbs.twimg.com/profile_images/731445765810200576/6VHrAllA_400x400.jpg",
+    "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  }
+];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,45 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+var friends = require("../data/friends");
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+  app.get("/api/tables", function(req, res) {
+    res.json(friends);
+  });
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the friends array)
+  // ---------------------------------------------------------------------------
+  app.post("/api/tables", function(req, res) {
+  // This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+  //   if (friends.length < 5) {
+  //     friends.push(req.body);
+  //     res.json(true);
+  //   }
+  //   else {
+  //     waitListData.push(req.body);
+  //     res.json(false);
+  //   }
+  // });
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+  app.post("/api/clear", function() {
+    // Empty out the arrays of data
+    friends = [];
+    console.log(friends);
+  });
+};
