98c06f261d6e39edb88c5d872ef268007848934a
Nate Villegas <natemv@gmail.com>
2017-08-02 17:18:59 -0700

    fix modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friendsArray = [
  {
     "name":"Nate",
     "photo":"https://pbs.twimg.com/profile_images/731445765810200576/6VHrAllA_400x400.jpg",
-    "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    "scores":["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
   }
 ];
 
